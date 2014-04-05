f75040b943aae74aad27bcd5cb5741b79b64de75
Robert Correiro <robertcorreiro@gmail.com>
2014-04-05 17:11:06 -0400

    implement basic request parsing

--- src/request.c
+++ src/request.c
@@ -1,66 +1,136 @@
-#include "dispatch.h"
-#include "request.h"
-#include <string.h>
+/*
 
-char *header_names[] = {
-  "Accept",
-  "Accept_Charset",
-  "Accept_Encoding",
-  "Accept_Language",
-  "Accept_Datetime",
-  "Authorization",
-  "Cache_Control",
-  "Cookie",
-  "Content_Length",
-  "Content_MD5",
-  "Content_type",
-  "Date",
-  "Expect",
-  "From",
-  "Host",
-  "If_Match",
-  "If_Modified_Since",
-  "If_None_Match",
-  "If_Range",
-  "If_Unmodified_Since",
-  "Max-Forwards",
-  "Origin",
-  "Pragma",
-  "Proxy_Authorization",
-  "Range",
-  "Referer",
-  "TE",
-  "User_Agent",
-  "Via",
-  "Warning"
-};
+  request.c
+  =========
+    
+  Authors: Kyle Poore, Robert Correiro
+ 
+  Functions to parse HTTP request headers.
+ 
+*/
 
-request parse_request(char *buffer,int buflen){
-  int i,j;
-  int offset = 0;
-  request req;
-  int verb_length = strchr(buffer,' ') - buffer;
-  strncpy(req.method,buffer,(METHOD_NAME_LENGTH > verb_length) ? verb_length : METHOD_NAME_LENGTH);
-  offset += verb_length+1;
-  int uri_length = strchr(buffer + offset,' ') - (buffer + offset);
-  strncpy(req.URI,buffer+offset,(uri_length > 256) ? 256 : uri_length);
-  offset += uri_length+1;
-  int http_length = strchr(buffer + offset,'\n') - (buffer + offset);
-  strncpy(req.version,buffer+offset,(http_length > HTTP_VERSION_LENGTH)?HTTP_VERSION_LENGTH:http_length);
-  offset += http_length+1;
-  for(i=0;i<16;i++){
-    int line_length = strchr(buffer + offset,'\n') - (buffer + offset);
-    if(line_length < 2) {offset += 2; break;}
-    int name_length = strchr(buffer + offset,':') - (buffer + offset);
-    for(j=0;j<num_headers;j++){
-      if(!strncmp(header_names[j],buffer + offset,name_length)){
-        req.headers[i].type = j;
-        req.headers[i].value = buffer + offset;
-      }
-    }
-    offset += line_length + 1;
+#include "request.h"  /* MAX_REQ_LINE_LENGTH  constant*/
+#include "helpers.h"  /* readline(), strip() helpers */
+
+#include <stdio.h>  /* printf() */
+#include <string.h>  /* strlen() */
+#include <unistd.h>  /* ssize_t data type */
+//#include <string.h>
+//#include <stdlib.h>
+
+// char *header_names[] = {
+//   "Accept",
+//   "Accept_Charset",
+//   "Accept_Encoding",
+//   "Accept_Language",
+//   "Accept_Datetime",
+//   "Authorization",
+//   "Cache_Control",
+//   "Cookie",
+//   "Content_Length",
+//   "Content_MD5",
+//   "Content_type",
+//   "Date",
+//   "Expect",
+//   "From",
+//   "Host",
+//   "If_Match",
+//   "If_Modified_Since",
+//   "If_None_Match",
+//   "If_Range",
+//   "If_Unmodified_Since",
+//   "Max-Forwards",
+//   "Origin",
+//   "Pragma",
+//   "Proxy_Authorization",
+//   "Range",
+//   "Referer",
+//   "TE",
+//   "User_Agent",
+//   "Via",
+//   "Warning"
+// };
+
+int parse_req_line(char *buffer, request *req) {
+  /* Parse HTTP verb */
+  if (!strncmp("GET ", buffer, 4)) {
+    req->method = GET;
+    buffer += 4;
   }
-  req.body = buffer + offset;
-  req.body_len = buflen - offset;
-  return req;
+  else if (!strncmp("POST ", buffer, 5)) {
+    req->method = POST;
+    buffer += 5;
+  }
+  else {
+    req->method = UNSUPPORTED;
+    req->status = 501;
+    return -1;
+  }
+
+  /* Seek buffer to start of URI */
+  while (*buffer && isspace(*buffer)) {
+    buffer++;
+  }
+
 }
+
+void parse_header_line(char *buffer, request *req) {
+  // TODO: change to int, so you can check return code in parse_request
+
+}
+
+int parse_request(int conn_fd, request *req) {
+  char buffer[MAX_REQ_LINE_LENGTH];
+  int first_line = 1;
+  // printf("About to readline()...\n");
+  ssize_t tmp;
+  /* TODO: change while condition to terminate when we've parsed all lines 
+     relevant to us, instead of going through each line */
+  while ((tmp = readline(conn_fd, buffer, MAX_REQ_LINE_LENGTH - 1)) > 0) {
+    strip(buffer);
+    printf("buffer [%zu]: %s\n", strlen(buffer), buffer);
+    if (first_line) {
+      if (parse_req_line(buffer, req)) {
+        break;
+      }
+      first_line--;  /* turn flag off */
+    }
+    else {
+      parse_header_line(buffer, req);  
+    }
+  }
+
+  //readline(conn_fd, buffer, MAX_REQ_LINE_LENGTH - 1);
+  //printf("buffer: %s\n", buffer);
+}
+
+
+// request parse_request(char *buffer,int buflen){
+//   int i,j;
+//   int offset = 0;
+//   request req;
+//   int verb_length = strchr(buffer,' ') - buffer;
+//   strncpy(req.method,buffer,(METHOD_NAME_LENGTH > verb_length) ? verb_length : METHOD_NAME_LENGTH);
+//   offset += verb_length+1;
+//   int uri_length = strchr(buffer + offset,' ') - (buffer + offset);
+//   strncpy(req.URI,buffer+offset,(uri_length > 256) ? 256 : uri_length);
+//   offset += uri_length+1;
+//   int http_length = strchr(buffer + offset,'\n') - (buffer + offset);
+//   strncpy(req.version,buffer+offset,(http_length > HTTP_VERSION_LENGTH)?HTTP_VERSION_LENGTH:http_length);
+//   offset += http_length+1;
+//   for(i=0;i<16;i++){
+//     int line_length = strchr(buffer + offset,'\n') - (buffer + offset);
+//     if(line_length < 2) {offset += 2; break;}
+//     int name_length = strchr(buffer + offset,':') - (buffer + offset);
+//     for(j=0;j<num_headers;j++){
+//       if(!strncmp(header_names[j],buffer + offset,name_length)){
+//         req.headers[i].type = j;
+//         req.headers[i].value = buffer + offset;
+//       }
+//     }
+//     offset += line_length + 1;
+//   }
+//   req.body = buffer + offset;
+//   req.body_len = buflen - offset;
+//   return req;
+// }
